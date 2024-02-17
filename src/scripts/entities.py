a052f5af14680c0cdfe51b2bf678edbae3896b03
Bryan Ramos <bryan@ramos.codes>
2024-02-16 20:21:22 -0500

    wip

---
+++ src/scripts/entities.py
@@ -0,0 +1,8 @@
+import pygame
+
+class PhysicsEntity:
+    def __init__(self, game, e_type, pos, size):
+        self.game = game
+        self.type = e_type
+        self.pos = list(pos)
+        self.size = size
