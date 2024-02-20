c4900f63cd66ecec5d0bc21bacbd50d56b6e99a8
Bryan Ramos <bryan@ramos.codes>
2024-02-20 07:17:35 -0500

    wip

--- src/scripts/tilemap.py
+++ src/scripts/tilemap.py
@@ -10,7 +10,7 @@ class Tilemap:
         self.tilemap = {}
         self.offgrid_tiles = []
 
-        for i in range(10):
+        for i in range(200):
             self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
             self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
 
